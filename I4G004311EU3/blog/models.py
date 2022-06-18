16d4e63eebfa4ec94737bcf292467f67daf2efb7
Toby <tobyabraham10@gmail.com>
2022-06-18 23:19:18 +0100

    added

---
+++ I4G004311EU3/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField()
+    Author=
+    Created_date=models.DateTimeField()
+    Published_date=models.DateTimeField()
